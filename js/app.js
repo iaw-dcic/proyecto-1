458435c9a7ea2e5fcb8fbf9426f7424ad662330e
diegoorbe96 <dieorbe96sw@gmail.com>
2020-04-15 18:10:32 -0300

    Estructura basica de la pag
    -Agregue la estructura principal
    -Cree por codigo la tabla donde estan los botones
    
    TODO:
    -Expandir el tamaño de los botones en cada cuadrado

--- js/app.js
+++ js/app.js
@@ -1,1 +1,33 @@
+function crear_tabla() {
+    var tbl = document.getElementById('tabla_juego')
+    var tbody = document.createElement('tbody')
+        //Creo 5 filas y 5 columnas.
+    for (var i = 0; i < 5; i++) {
+        var tr = document.createElement('tr');
+        for (var j = 0; j < 5; j++) {
+            var td = document.createElement('td');
+            //Agrego el boton a cada uno.
+            var but = crear_boton_tabla();
+            td.appendChild(but)
+            tr.appendChild(td);
+        }
+        tbody.appendChild(tr);
+    }
+    tbl.appendChild(tbody)
+}
 
+
+function crear_boton_tabla() {
+    var salida = document.createElement('button')
+
+    return salida
+
+
+
+
+
+}
+
+
+
+crear_tabla()
