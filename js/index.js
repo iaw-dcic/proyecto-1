d5cf347d234ef4f4bdfb15448c1bbaa4a2a024d7
ajavieralejandro <amorosijavier@gmail.com>
2020-04-16 01:45:16 -0300

    insertar elementos en un arreglo aleatoriamente sin repeticiones,oK

--- js/index.js
+++ js/index.js
@@ -1,7 +1,12 @@
 $(document).ready(function() {
   var aux = "";
+  var defaultImage;
   var hideComponent;
   var cont = 0;
+  let array = generateMemo();
+  console.log("El array generado es : ", array);
+
+  //Tengo que inicializar un mapeo...
 
   //Función que chequea que pasa cada vez que hago click en una carta
   $(".card").click(function() {
@@ -27,3 +32,39 @@ $(document).ready(function() {
     }
   });
 });
+
+function generateMemo() {
+  //arreglo con los elementos a insertar
+  let arrayToIns = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+  //arreglo con las posiciones libres
+  let posiciones = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+  let toR = [];
+
+  console.log("Testeando el arreglo de posiciones");
+  let aux = getPosicion(posiciones);
+  console.log("La posicion seleccionada es : " + aux);
+  arrayToIns.forEach(element => {
+    //obteno la posicion a insertar
+    console.log("El arreglo de posiciones ahora es  ", posiciones);
+    let pos = getPosicion(posiciones);
+    //ahora tengo que eliminar pos del arreglo de posiciones
+    posiciones = posiciones.filter(element => element !== pos);
+    //inserto el elemento en la posicion a insertar
+    toR[pos] = element;
+  });
+
+  return toR;
+}
+
+//Genera la posicion en base a un arreglo que le es pasado por parametros
+function getPosicion(array) {
+  let x = Math.floor(Math.random() * array.length);
+  //console.log("x es : " + x);
+  let min = array[0];
+  array.forEach(element => {
+    if (Math.abs(element - x) < Math.abs(min - x)) min = element;
+    //console.log("Ahora min es : " + min);
+  });
+
+  return min;
+}
