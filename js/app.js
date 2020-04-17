15a61a8c1477e884c08f7c27e3b5354f8a53db6d
Danilo Pesce <danilo9.p@hotmail.com>
2020-04-16 22:03:27 -0300

    Prueba de archivo js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,33 @@
 
+function verificar() {
+  //acá tengo que verificar completamente la contraseña
+  //puedo devolver un arrego con todos los valores que necesito
+  console.log("probando");
+}
+
+function contarMinusculas(text) {
+
+}
+
+function contarMayusculas(text) {
+
+}
+
+function contarLetras(text) {
+
+}
+
+function contarNumeros(text) {
+
+}
+
+function contarSimbolos(text) {
+
+}
+
+function cumpleRequisitosMinimos(text) {
+
+}
+
+
+
