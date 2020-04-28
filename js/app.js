4ca5722b72658c6e4effa7a7294b5adeb13e23eb
matiKloster <matikloster2@gmail.com>
2020-04-28 15:17:34 -0300

    Lista de palabras

--- js/app.js
+++ js/app.js
@@ -1,25 +1,38 @@
-var ws= wordsearch(['aaaaa'],5,5 );
 
-function dibujarGrilla(/*el,*/ puzzle) {
-      
+var words = ['tupac', 'eazy', 'biggie', 'nas'];
+var ws = wordsearch(words, 6, 6);
+
+function getHTMLgrilla(puzzle) {
+
   var output = '';
   // for each row in the puzzle
   for (var i = 0, height = puzzle.length; i < height; i++) {
     // append a div to represent a row in the puzzle
     var row = puzzle[i];
-    output += '<div>';
+    output += '<div class="row justify-content-center">';
     // for each element in that row
     for (var j = 0, width = row.length; j < width; j++) {
-        // append our button with the appropriate class
-        output += '<button class="botonGrilla" x="' + j + '" y="' + i + '">';
-        output += row[j] || '&nbsp;';
-        output += '</button>';
+      output += '<div class="cols-2">';
+      // append our button with the appropriate class
+      output += '<button class="botonGrilla" x="' + j + '" y="' + i + '">';
+      output += row[j] || '&nbsp;';
+      output += '</button></div>';
     }
     // close our div that represents a row
     output += '</div>';
   }
   return output;
-  //$(el).html(output);
 }
-$('#Grilla').html(dibujarGrilla(ws.grid));
+
+function getHTMLPalabras(palabras) {
+  var output = '<ul class="list-group">';
+  for (var i = 0; i < palabras.length; i++){
+    output += '<li class="list-group-item">' + palabras[i].toUpperCase() + '</li>';
+  }
+  output += '</ul>';
+  return output;
+}
+
+$('#Grilla').html(getHTMLgrilla(ws.grid));
+$('#Palabras').html(getHTMLPalabras(words));
 
