c373ac572d5d53c1ef128c090e4a0fbb6bdfaa4a
matiKloster <matikloster2@gmail.com>
2020-04-28 12:25:38 -0300

    Servicio accedido. Grilla funcionando. Bootstrap y jQuery.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,25 @@
+var ws= wordsearch(['aaaaa'],5,5 );
+
+function dibujarGrilla(/*el,*/ puzzle) {
+      
+  var output = '';
+  // for each row in the puzzle
+  for (var i = 0, height = puzzle.length; i < height; i++) {
+    // append a div to represent a row in the puzzle
+    var row = puzzle[i];
+    output += '<div>';
+    // for each element in that row
+    for (var j = 0, width = row.length; j < width; j++) {
+        // append our button with the appropriate class
+        output += '<button class="botonGrilla" x="' + j + '" y="' + i + '">';
+        output += row[j] || '&nbsp;';
+        output += '</button>';
+    }
+    // close our div that represents a row
+    output += '</div>';
+  }
+  return output;
+  //$(el).html(output);
+}
+$('#Grilla').html(dibujarGrilla(ws.grid));
 
---
+++ js/asdasd.js
@@ -0,0 +1,4 @@
+var ws=wordsearch(["hola","mati","sos","recontra","capo"],10,10);
+ws.grid.forEach(element => {
+    document.write(element+"<br/>");
+});
