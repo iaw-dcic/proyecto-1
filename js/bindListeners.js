672ed3ff5ae5cc0688fb13f74b8f94b26d3be91b
matiKloster <matikloster2@gmail.com>
2020-05-02 05:03:54 -0300

    Solucionado bug del toggle Mode

--- js/bindListeners.js
+++ js/bindListeners.js
@@ -1,7 +1,7 @@
 $(".botonGrilla").mousedown(empiezaSeleccion);
 $(".botonGrilla").mouseenter(mouseMove);
 $(".botonGrilla").mouseup(finalizaSeleccion);
+$("#switch").mousedown(toggleMode);
 $(".botonGrilla").on("touchstart", startTurn);
 $(".botonGrilla").on("touchmove", touchMove);
 $(".botonGrilla").on("touchend", endTurn);  
-$("#switch").mousedown(toggleMode);
