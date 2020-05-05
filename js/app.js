e2e1d84078c1f090c0d4ae964bb7e6343f16e93f
Agustin <agustinoq@github.com>
2020-05-05 16:32:18 -0300

    animaciones y handlers para los botones

--- js/app.js
+++ js/app.js
@@ -1,3 +1,27 @@
-$(document).ready(function () {
-
+$(document).ready(function() {
+add_handlers();
 });
+
+function add_handlers() {
+  $(".simon_btn").on("click",btn_press);
+}
+
+function btn_press() {
+  let boton = $(this).attr('id');
+  animateCSS("#"+boton,"pulse");
+
+}
+
+function animateCSS(element, animationName, callback) {
+    const node = document.querySelector(element)
+    node.classList.add('animated', animationName)
+
+    function handleAnimationEnd() {
+        node.classList.remove('animated', animationName)
+        node.removeEventListener('animationend', handleAnimationEnd)
+
+        if (typeof callback === 'function') callback()
+    }
+
+    node.addEventListener('animationend', handleAnimationEnd)
+}
