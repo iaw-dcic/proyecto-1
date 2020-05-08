f21d88007e0e1faecf4c360412e40d5150afd40e
Stefano <stefano_canossini@hotmail.com>
2020-05-08 05:08:36 -0300

    funcion chequear primo y UI

--- js/app.js
+++ js/app.js
@@ -1,3 +1,46 @@
-
 // next prime geeksforgeeks
 // previous prime algorithm
+var entrada = document.getElementById("entrada");
+var textoResultado = document.getElementById("resultado");
+var n, res;
+
+entrada.addEventListener("keydown", function (e) {
+    if (e.keyCode === 13){ //controla que la tecla presionada sea enter
+        n = entrada.value;
+        res = esPrimo(n);
+        asignarTextoResultado(res);
+        colorTextoResultado(res);
+    }
+});
+
+function esPrimo(n){
+    if (n <= 1)  return  false;  
+    if (n <= 3)  return true;  
+    
+    // This is checked so that we can skip   
+    // middle five numbers in below loop
+
+    if (n % 2 == 0 || n % 3 == 0)
+        return false;  
+    
+    for (var i = 5; i * i <= n; i = i + 6)  
+        if (n % i == 0 || n % (i + 2) == 0)  
+            return false;  
+
+    return true;  
+} 
+
+function asignarTextoResultado(bool){
+    if(bool == true)
+        textoResultado.innerHTML = "es primo";
+    else
+        textoResultado.innerHTML = "no es primo";
+}
+
+function colorTextoResultado(bool){
+    if(bool == true) 
+            textoResultado.className = "verdadero";
+        else
+            textoResultado.className = "falso";
+
+}
