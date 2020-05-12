9911ea4c37e25b83703d7f2cbf956111d8a7a3fe
FacundoMarin <FacundoMarin@users.noreply.github.com>
2020-05-12 16:45:31 -0300

    Ajustes visuales y refactor

--- js/ShapeGeneration.js
+++ js/ShapeGeneration.js
@@ -1,125 +1,169 @@
-var elem = document.getElementById("twoWrapper");
-var two = new Two({ type: Two.Types.canvas, height: 600, width: 800 }).appendTo(
-  elem
-);
+//Generation constants: 
 
-two.renderer.domElement.id = "canvas";
+var NUMBER_OF_LAYERS;
+var LAYER_RADIUS = 50;
+var MIN_SHAPES_PER_LAYER;
+var MAX_SHAPES_PER_LAYER;
+var ORBIT_SHAPE_PROBABILITY = .2;
+var TWIN_SHAPE_PROBABILITY = .2;
+var INSIDE_LINES_PROBABILITY = .1;
+var ANIMATION_MULTIPLIER;
+//-------------------------------------
 
+
+//number of groups used for animation
+var animationGroupsQuantity = 7;
+//reference to animation groups
+var animationGroups;
+//used to avoid animation when groups haven't been created
+var groupsGenerated = false;
+
+//Center values used for shape generation
 var centerX = 0;
 var centerY = 0;
 
-var groupsQuantity = 7;
-var groups;
+//-------------------------------------
 
-var NUMBER_OF_LAYERS;
-var MAX_SHAPES_PER_LAYER;
-var MIN_SHAPES_PER_LAYER;
-var ANIMATION_SPEED;
+//Initialization code
 
-function generate() {
-  two.clear();
-  startGroups();
-  for (let i = NUMBER_OF_LAYERS; i > 0; i--) {
-    createLayer(centerX, centerY, 30 * i);
-  }
-  for (let i = 0; i < groups.length; i++) {
-    groups[i].translation.set(two.width / 2, two.height / 2);
-  }
-}
-
-function startGroups() {
-  groups = new Array(groupsQuantity);
-  for (let i = 0; i < groups.length; i++) {
-    groups[i] = two.makeGroup();
-  }
-}
+//Set up of canvas and 'two' object
+var elem = document.getElementById("twoWrapper");
+var two = new Two({ type: Two.Types.canvas, fullscreen: true}).appendTo(
+  elem
+);
+//we set the id of the created element for reference.
+two.renderer.domElement.id = "canvas";
 
 // Bind a function to scale and rotate the group
 // to the animation loop.
-two
-  .bind("update", function (_frameCount) {
-    if (ANIMATION_SPEED != 0) {
-      var animationMultiplier = ANIMATION_SPEED * Math.PI;
-      groups[0].rotation += animationMultiplier * 0.002;
-      groups[1].rotation -= animationMultiplier * 0.001;
-      groups[2].rotation -= animationMultiplier * 0.002;
-      groups[3].rotation += animationMultiplier * 0.001;
-      groups[4].rotation -= animationMultiplier * 0.0005;
-      groups[5].rotation += animationMultiplier * 0.0005;
+two.bind("update", function (_frameCount) {
+    if (ANIMATION_MULTIPLIER != 0 && groupsGenerated) {
+      var animationSpeed = ANIMATION_MULTIPLIER * Math.PI;
+      animationGroups[0].rotation += animationSpeed * 0.002;
+      animationGroups[1].rotation -= animationSpeed * 0.001;
+      animationGroups[2].rotation -= animationSpeed * 0.002;
+      animationGroups[3].rotation += animationSpeed * 0.001;
+      animationGroups[4].rotation -= animationSpeed * 0.0005;
+      animationGroups[5].rotation += animationSpeed * 0.0005;
     }
-  })
-  .play(); // Finally, start the animation loop
+  }).play(); // Finally, start the animation loop
+
+//---------------------------------------
+
+//FUNCTIONS:
+
+function generate() {
+  two.clear();
+  startAnimationGroups();
+  for (let i = NUMBER_OF_LAYERS; i > 0; i--) {
+    createLayer(centerX, centerY, LAYER_RADIUS * i);
+  }
+
+  //all shapes are created around 0,0 so we center the entire groups
+  for (let i = 0; i < animationGroups.length; i++) {
+    animationGroups[i].translation.set(two.width / 2, two.height / 2);
+  }
+  groupsGenerated = true;
+}
+
+function startAnimationGroups() {
+  animationGroups = new Array(animationGroupsQuantity);
+  for (let i = 0; i < animationGroups.length; i++) {
+    animationGroups[i] = two.makeGroup();
+  }
+}
 
 function createLayer(xValue, yValue, referenceRadius) {
-  var shapesPerLayer =
-    Math.floor(
-      Math.random() * (+MAX_SHAPES_PER_LAYER + 1 - +MIN_SHAPES_PER_LAYER)
-    ) + +MIN_SHAPES_PER_LAYER;
-  var newShape;
+  var shapesPerLayer = randomInRange(MIN_SHAPES_PER_LAYER, MAX_SHAPES_PER_LAYER);
   for (let i = 0; i < shapesPerLayer; i++) {
-    newShape = createShape(xValue, yValue, referenceRadius);
-    if (Math.random() > 0.5) {
-      createTwinShape(newShape, 0.97);
-    }
-    if (Math.random() > 0.5) {
-      createTwinShape(newShape, 1.03);
-    }
+    createShape(xValue, yValue, referenceRadius);
   }
-  if (Math.random() > 0.8) {
+  if (Math.random() > 1 - ORBIT_SHAPE_PROBABILITY) {
     createOrbitShapes(referenceRadius);
   }
 }
 
 function createShape(xValue, yValue, referenceRadius) {
-  var shapeType = Math.floor(Math.random() * (4 - 1)) + 1;
-  var newShape;
+  var shapeType = randomInRange(1,4);
   switch (shapeType) {
     case 1:
-      newShape = createCircle(xValue, yValue, referenceRadius);
+      createCircle(xValue, yValue, referenceRadius);
       break;
     case 2:
-      newShape = createPolygon(xValue, yValue, referenceRadius, 3);
+      createPolygon(xValue, yValue, referenceRadius, 3);
       break;
     case 3:
-      newShape = createPolygon(xValue, yValue, referenceRadius, 6);
+      createPolygon(xValue, yValue, referenceRadius, 6);
       break;
     default:
       break;
   }
-  return newShape;
 }
 
 function createCircle(xValue, yValue, referenceRadius) {
-  var circle = two.makeCircle(xValue, yValue, referenceRadius);
-  addStyle(circle);
-  assignToGroup(circle);
-  return circle;
+  var circleGroup = two.makeGroup();
+  
+  circleGroup.add(two.makeCircle(xValue, yValue, referenceRadius));
+
+  //a chance to create a double or triple circle stroke 
+  if (Math.random() > 1 - TWIN_SHAPE_PROBABILITY) {
+    circleGroup.add(two.makeCircle(xValue, yValue, referenceRadius * 1.03));  
+  }
+  if (Math.random() > 1 - TWIN_SHAPE_PROBABILITY) {
+    circleGroup.add(two.makeCircle(xValue, yValue, referenceRadius * .97));  
+  }
+
+  addStyle(circleGroup);
+  assignToAnimationGroup(circleGroup);
 }
 
 function createPolygon(xValue, yValue, referenceRadius, sides) {
   var polygonGroup = two.makeGroup();
-  if (Math.random() > 0.9) {
-    var angle = (2 * Math.PI) / sides;
-    var offsetAngle = Math.PI / sides;
-    for (let i = 0; i < sides; i++) {
-      var newLine = new Two.Line(
-        centerX - referenceRadius * Math.sin(offsetAngle + angle * i),
-        centerY + referenceRadius * Math.cos(offsetAngle + angle * i),
-        centerX,
-        centerY
-      );
-      two.add(newLine);
-      polygonGroup.add(newLine);
-    }
+
+  //a chance to draw lines from the vertices of the polygon to its center
+  if (Math.random() > 1 - INSIDE_LINES_PROBABILITY) {
+    polygonGroup.add(createInsideLines(xValue, yValue, referenceRadius, sides));
   }
-  var polygon = two.makePolygon(xValue, yValue, referenceRadius, sides);
-  polygonGroup.add(polygon);
+
+  //a chance to create a double or triple polygon stroke 
+  if (Math.random() > 1 - TWIN_SHAPE_PROBABILITY) {
+    polygonGroup.add(two.makePolygon(xValue, yValue, referenceRadius * 1.03, sides));
+  }
+  if (Math.random() > 1 - TWIN_SHAPE_PROBABILITY) {
+    polygonGroup.add(two.makePolygon(xValue, yValue, referenceRadius * .97, sides));
+  }
+
+  //we create the shape itself
+  polygonGroup.add(two.makePolygon(xValue, yValue, referenceRadius, sides));
+
+  //half the times the entire shape is rotated for more variation
   if (Math.random() > 0.5) {
     polygonGroup.rotation += Math.PI / sides;
   }
   addStyle(polygonGroup);
-  assignToGroup(polygonGroup);
-  return polygon;
+  assignToAnimationGroup(polygonGroup);
+}
+
+function createInsideLines(xValue, yValue, referenceRadius, sides) {
+  var linesGroup = two.makeGroup();
+
+  //the angle between the lines
+  var angle = (2 * Math.PI) / sides;
+  //we need an offset angle due to the way the library creates shapes
+  var offsetAngle = angle / 2;
+  for (let i = 0; i < sides; i++) {
+    //we use the parametric function of the circle to set the values around the circumference
+    //so the first x,y pair is on the circunference, and the second is at the center of the figure
+    var newLine = new Two.Line(
+      xValue - referenceRadius * Math.sin(offsetAngle + angle * i),
+      yValue + referenceRadius * Math.cos(offsetAngle + angle * i),
+      xValue,
+      yValue
+    );
+    two.add(newLine);
+    linesGroup.add(newLine);
+  }
+  return linesGroup;
 }
 
 function createOrbitShapes(orbitRadius) {
@@ -129,36 +173,35 @@ function createOrbitShapes(orbitRadius) {
 }
 
 function createCircles(n, radius) {
+  //angle between the circles
   var angle = (2 * Math.PI) / n;
-  var circles = new Array(n);
+  var newCircle;
   var circlesGroup = two.makeGroup();
   for (let i = 0; i < n; i++) {
-    circles[i] = two.makeCircle(
+    //we use the parametric function of the circle to place them around the circumference
+    newCircle = two.makeCircle(
       centerX + radius * Math.sin(angle * i),
       centerY + radius * Math.cos(angle * i),
       radius * 0.1
     );
-    circlesGroup.add(circles[i]);
+    circlesGroup.add(newCircle);
   }
   addStyle(circlesGroup);
-  assignToGroup(circlesGroup);
-}
-
-function createTwinShape(shape, scale) {
-  var newShape = shape.clone();
-  newShape.scale = shape.scale * scale;
-  two.add(newShape);
-  assignToGroup(newShape);
-  return newShape;
+  assignToAnimationGroup(circlesGroup);
 }
 
 function addStyle(shape) {
   shape.fill = "rgba(255, 255, 255, .07)";
   shape.stroke = "rgba(0,0,0,1)";
-  shape.linewidth = (Math.floor(Math.random() * 3) + 1) / 3;
+  shape.linewidth = randomInRange(1,3) / 3;
 }
 
-function assignToGroup(shape) {
-  var groupIndex = Math.floor(Math.random() * groups.length);
-  groups[groupIndex].add(shape);
+function assignToAnimationGroup(shape) {
+  var groupIndex = randomInRange(0, animationGroups.length);
+  animationGroups[groupIndex].add(shape);
+}
+
+//Returns random int between two values. min inclusive. max exclusive
+function randomInRange(minVal, maxVal) {
+  return Math.floor(Math.random() * (maxVal - minVal)) + minVal;
 }
--- js/app.js
+++ js/app.js
@@ -3,13 +3,29 @@ var minShapesPerLayerInput = document.getElementById("minShapesPerLayerInput");
 var maxShapesPerLayerInput = document.getElementById("maxShapesPerLayerInput");
 var layersNumber = document.getElementById("layersNumber");
 var layersSlider = document.getElementById("layersSlider");
+var animationSpeedSlider = document.getElementById("animationSpeedSlider");
 var animationSpeedNumber = document.getElementById("animationSpeedNumber");
 var exportButton = document.getElementById("exportButton");
+var lightThemeButton = document.getElementById("lightThemeButton");
+var darkThemeButton = document.getElementById("darkThemeButton");
 
-generateButton.onclick = generateShapes;
-exportButton.onclick = exportCanvas;
+var lightTheme = 'css/lightTheme.css';
+var darkTheme = 'css/darkTheme.css';
+
+setListeners();
 loadData();
 setAnimationSpeed(0);
+
+function setListeners() {
+  generateButton.onclick = generateShapes;
+  exportButton.onclick = exportCanvas;
+  layersSlider.oninput = setNumberOfLayers;
+  minShapesPerLayerInput.oninput = setShapesPerLayer;
+  maxShapesPerLayerInput.oninput = setShapesPerLayer;
+  animationSpeedSlider.oninput = setAnimationSpeed;
+  lightThemeButton.onclick = function() { changeTheme(lightTheme); }
+  darkThemeButton.onclick = function() { changeTheme(darkTheme); }
+}
 
 function generateShapes() {
   generate();
@@ -20,15 +36,17 @@ function changeTheme(theme) {
   localStorage.setItem("theme", theme);
 }
 
-function setNumberOfLayers(value) {
+function setNumberOfLayers() {
+  var value = layersSlider.value;
   layersNumber.textContent = value;
   NUMBER_OF_LAYERS = value;
   localStorage.setItem("numberOfLayers", value);
 }
 
-function setAnimationSpeed(value) {
+function setAnimationSpeed() {
+  var value = animationSpeedSlider.value;
   animationSpeedNumber.textContent = value / 10;
-  ANIMATION_SPEED = value / 10;
+  ANIMATION_MULTIPLIER = value / 10;
 }
 
 function setShapesPerLayer() {
@@ -39,15 +57,34 @@ function setShapesPerLayer() {
 }
 
 function loadData() {
-  MIN_SHAPES_PER_LAYER = +localStorage.getItem("minShapesPerLayer");
-  minShapesPerLayerInput.value = MIN_SHAPES_PER_LAYER;
-  MAX_SHAPES_PER_LAYER = +localStorage.getItem("maxShapesPerLayer");
-  maxShapesPerLayerInput.value = MAX_SHAPES_PER_LAYER;
-  NUMBER_OF_LAYERS = localStorage.getItem("numberOfLayers");
-  layersNumber.textContent = NUMBER_OF_LAYERS;
-  layersSlider.value = NUMBER_OF_LAYERS;
-
-  document.getElementById("theme").href = localStorage.getItem("theme");
+  if (localStorage.getItem("minShapesPerLayer")) {
+    MIN_SHAPES_PER_LAYER = +localStorage.getItem("minShapesPerLayer");
+    minShapesPerLayerInput.value = MIN_SHAPES_PER_LAYER;
+  } else {
+    MIN_SHAPES_PER_LAYER = 1;
+    minShapesPerLayerInput.value = MIN_SHAPES_PER_LAYER;
+  }
+  if (localStorage.getItem("maxShapesPerLayer")) {
+    MAX_SHAPES_PER_LAYER = +localStorage.getItem("maxShapesPerLayer");
+    maxShapesPerLayerInput.value = MAX_SHAPES_PER_LAYER;
+  } else {
+    MAX_SHAPES_PER_LAYER = 2;
+    maxShapesPerLayerInput.value = MAX_SHAPES_PER_LAYER;
+  }
+  if (localStorage.getItem("numberOfLayers")) {
+    NUMBER_OF_LAYERS = localStorage.getItem("numberOfLayers");
+    layersNumber.textContent = NUMBER_OF_LAYERS;
+    layersSlider.value = NUMBER_OF_LAYERS;
+  } else {
+    NUMBER_OF_LAYERS = 2;
+    layersNumber.textContent = NUMBER_OF_LAYERS;
+    layersSlider.value = NUMBER_OF_LAYERS;
+  }
+  if (localStorage.getItem("theme")) {
+    document.getElementById("theme").href = localStorage.getItem("theme");
+  } else {
+    document.getElementById("theme").href = lightTheme;
+  }
 }
 
 function exportCanvas() {
