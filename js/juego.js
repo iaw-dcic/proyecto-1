0de7f3403ee4747ddebdcc75ee37440b0a0fa492
leandro mara <leandrodavidm@hotmail.com>
2020-05-26 18:32:10 -0300

    Re entrega

---
+++ js/juego.js
@@ -0,0 +1,184 @@
+var fondoJuego;
+var pelota;
+var flechaIzquierda;
+var flechaDerecha;
+var marcadorLocal = 0;
+var rivales = 12;
+var cartel;
+var cartel2;
+var velocidad = 200;
+var goles = 0;
+var arcoLibre;
+var partidoFutbol = new Phaser.Game(405, 526, Phaser.AUTO, 'cancha');
+
+var playing = false;
+var startButton;
+
+
+
+
+//  Carga recursos del juego
+var estadoPrincipal = {
+
+    preload: function() {
+        partidoFutbol.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+        partidoFutbol.scale.pageAlignHorizontally = true;
+        partidoFutbol.scale.pageAlignVertically = true;
+        partidoFutbol.load.image('cesped', 'img/cesped.jpeg');
+        partidoFutbol.load.image('pelota', 'img/pelota2.png');
+        partidoFutbol.load.image('pie', 'img/pie.png');
+
+    },
+    //Mostrar los recursos cargados por patalla
+    create: function() {
+
+        fondoJuego = partidoFutbol.add.tileSprite(0, 0, 405, 529, 'cesped');
+        pelota = partidoFutbol.add.sprite(200, 250, 'pelota');
+        pie = partidoFutbol.add.sprite(160, 480, 'pie');
+
+        pelota.anchor.setTo(0.5);
+        pelota.scale.setTo(0.15, 0.15);
+        flechaDerecha = partidoFutbol.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
+        flechaIzquierda = partidoFutbol.input.keyboard.addKey(Phaser.Keyboard.LEFT);
+        partidoFutbol.physics.startSystem(Phaser.Physics.ARCADE);
+        partidoFutbol.physics.arcade.enable(pelota);
+        partidoFutbol.physics.arcade.enable(pie);
+
+
+        cartel2 = partidoFutbol.add.text(20, 280, 'Goles: 0', { font: '35px Arial', fill: 'rgb(63, 29, 29)' });
+        pie.body.collideWorldBounds = true;
+
+        pie.body.immovable = true;
+
+        pelota.body.velocity.set(velocidad, velocidad);
+        pelota.body.collideWorldBounds = true;
+        partidoFutbol.physics.arcade.checkCollision.down = false;
+        pelota.body.bounce.set(1);
+        pelota.checkWorldBounds = true;
+        partidoFutbol.paused = true;
+        pelota.events.onOutOfBounds.add(function() {
+            marcadorLocal += 1;
+            agregarresultado();
+            btnInicio.disabled = false;
+            partidoFutbol.state.start('partido');
+
+            alert(" Marcaste " + goles + " goles a tu Equipo rival ");
+
+        }, this);
+
+        iniciarRivales();
+
+    },
+
+    update: function() {
+
+        partidoFutbol.physics.arcade.collide(pelota, pie);
+        partidoFutbol.physics.arcade.collide(pelota, arcoLibre, gambetaArquero);
+        partidoFutbol.physics.arcade.collide(pelota, rivals, gambeta);
+
+        if (flechaDerecha.isDown) {
+            pie.position.x += 10;
+        }
+        if (flechaIzquierda.isDown) {
+            pie.position.x -= 10;
+        }
+
+    }
+
+
+
+}
+
+function iniciarRivales() {
+    rivalInfo = {
+        width: 50,
+        height: 20,
+        count: {
+            row: 3,
+            col: 4
+        },
+        offset: {
+            top: 50,
+            left: 80
+        },
+        padding: 30
+    }
+    rivals = partidoFutbol.add.group();
+    for (c = 0; c < rivalInfo.count.col; c++) {
+        for (r = 0; r < rivalInfo.count.row; r++) {
+            var rivalX = (c * (rivalInfo.width + rivalInfo.padding)) + rivalInfo.offset.left;
+            var rivalY = (r * (rivalInfo.height + rivalInfo.padding)) + rivalInfo.offset.top;
+            nuevoRival = partidoFutbol.add.sprite(rivalX, rivalY, 'pie');
+            partidoFutbol.physics.enable(nuevoRival, Phaser.Physics.ARCADE);
+            nuevoRival.body.immovable = true;
+            nuevoRival.anchor.set(0.5);
+            rivals.add(nuevoRival);
+        }
+    }
+}
+
+
+
+
+
+
+function cerrarArco() {
+    arcoLibre = partidoFutbol.add.sprite(200, 10, 'pie');
+    partidoFutbol.physics.enable(arcoLibre, Phaser.Physics.ARCADE);
+    arcoLibre.body.immovable = true;
+    partidoFutbol.physics.arcade.enable(arcoLibre);
+    arcoLibre.anchor.set(0.5);
+
+}
+
+
+
+function gambeta(pelota, rival) {
+
+    rival.kill();
+    rivales -= 1;
+    velocidad += 10;
+    pelota.body.velocity.set(velocidad, velocidad + 20);
+    if (rivales == 0) {
+
+        cerrarArco();
+
+    }
+
+}
+
+function gambetaArquero(pelota, rival) {
+
+    rival.kill();
+    goles += 1;
+    cartel2.setText('Goles: ' + goles);
+    velocidad += 40;
+    pelota.body.velocity.set(velocidad, velocidad + 30);
+    if (rivales < 1) {
+
+        cerrarArco();
+
+    }
+
+}
+
+function startGame() {
+    startButton.destroy();
+    pelota.body.velocity.set(150, -150);
+    playing = true;
+}
+
+function setValores() {
+    marcadorLocal = 0;
+    rivales = 12;
+    goles = 0;
+    if (nivel == 'acosta') {
+        velocidad = 300;
+    } else {
+        velocidad = 150;
+    }
+}
+
+
+partidoFutbol.state.add('partido', estadoPrincipal);
+partidoFutbol.state.start('partido');
---
+++ js/principal.js
@@ -0,0 +1,131 @@
+let nivel = 'acosta';
+var nombre = 'anonimo';
+var lista = [];
+
+
+//recupero la lista de los mejores 5 ususarios
+
+function recuperar() {
+    var listaJSON = localStorage.getItem("listas");
+    lista = JSON.parse(listaJSON);
+}
+
+//almaceno la lista de los mejores 5 ususarios
+
+function almacenar() {
+    var jsonLista = JSON.stringify(lista);
+    localStorage.setItem("listas", jsonLista);
+}
+
+function actualizarTabla() {
+    let res = document.querySelector('#res');
+    res.innerHTML = '';
+    lista.forEach(element => {
+        res.innerHTML += `
+                <tr>
+                <td>${element.nombre}</td>
+                <td>${element.goles}</td>
+                <tr>
+            `
+    });
+}
+
+function agregarresultado() {
+    if (lista === null) {
+        lista = [{ nombre: nombre, goles: goles }];
+    } else if (lista.length == 5) {
+        lista.pop();
+        lista.unshift({ nombre: nombre, goles: goles });
+    } else {
+        lista.unshift({ nombre: nombre, goles: goles });
+    }
+    almacenar();
+    actualizarTabla();
+}
+
+
+//funcion para registrar los usuarios, en caso de estar dentro de las mejores goleadas, 
+//el usuario quedara registrado en la tabla
+
+const btnRegistro = document.querySelector('#botonnombre');
+btnRegistro.addEventListener('click', () => {
+    nombre = document.getElementById('nombre').value;
+    if (nombre === '') {
+        nombre = 'anonimo';
+    }
+
+});
+
+const btnInicio = document.querySelector('#comenzar');
+btnInicio.addEventListener('click', () => {
+    btnInicio.disabled = true;
+    setValores();
+    partidoFutbol.paused = false;
+});
+
+
+
+
+
+
+//boton para cambiar el nivel(acosta) del juego 
+
+const btnAcosta = document.querySelector('#acosta');
+btnAcosta.addEventListener('click', () => {
+    let imagen = document.querySelector('#nivelimagen');
+    imagen.src = 'img/acosta.jpg';
+    nivel = 'acosta';
+    localStorage.setItem('nivel-acosta', 'true');
+});
+//boton para cambiar el nivel(juvenil) del juego 
+
+const btnJuvenil = document.querySelector('#juvenil');
+btnJuvenil.addEventListener('click', () => {
+    let imagen = document.querySelector('#nivelimagen');
+    imagen.src = 'img/juvenil.jpg';
+    nivel = 'juvenil';
+    localStorage.setItem('nivel-acosta', 'false');
+});
+
+//boton para hacer los cambios de estilo de la pagina
+
+const btnEstilo = document.querySelector('#estilo');
+btnEstilo.addEventListener('click', () => {
+    document.body.classList.toggle('grana');
+    btnEstilo.classList.toggle('activo');
+
+    if (document.body.classList.contains('grana')) {
+        localStorage.setItem('estilo-grana', 'true');
+    } else {
+        localStorage.setItem('estilo-grana', 'false')
+    }
+});
+
+// Setear ultimo estilo elegido Usuario
+
+if (localStorage.getItem('estilo-grana') === 'true') {
+    document.body.classList.add('grana');
+    btnEstilo.classList.add('activo');
+} else {
+    document.body.classList.remove('grana');
+    btnEstilo.classList.remove('activo');
+}
+
+//Setear ultimo nivel elegido Usuario
+
+if (localStorage.getItem('nivel-acosta') === 'true') {
+    let imagen = document.querySelector('#nivelimagen');
+    imagen.src = src = 'img/acosta.jpg';
+    nivel = 'acosta';
+} else {
+    let imagen = document.querySelector('#nivelimagen');
+    imagen.src = src = 'img/juvenil.jpg';
+    nivel = 'juvenil';
+}
+
+
+
+recuperar();
+if (lista !== null) {
+    actualizarTabla();
+}
